86994a9b2eef20c8f62a8fb55407e0d01329ec5a
tightcode <tightcodedev@gmail.com>
2019-05-30 00:08:16 -0700

    0.1.0 initial release

---
+++ centerslidenav.js
@@ -0,0 +1,49 @@
+/* Centerslidenav 0.1.0 ( https://github.com/tightcode/centerslidenav ) */
+// changed breakpoint from md to lg
+
+
+
+/* Slidenav 4.0.0-alpha.1 ( https://github.com/tightcode/slidenav ) */
+
+// CSS media feature is used to detect if the user has requested that the system minimize the amount of animation or motion it uses
+var mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
+var slideSpeed = 350;
+
+var slideSpeedInit = function () {
+    if (mediaQuery.matches) {
+        slideSpeed = 1;
+    } else {
+        slideSpeed = 350;
+    }
+};
+
+window.addEventListener("load", slideSpeedInit);
+mediaQuery.addEventListener("change", slideSpeedInit);
+
+// Jquery slide effect
+$('.jq-slide').on('show.bs.dropdown', function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).slideDown(slideSpeed);
+});
+$('.jq-slide').on('hide.bs.dropdown', function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).slideUp(slideSpeed);
+});
+
+// JqueryUI slide effect
+$('.jqui-slide').on('show.bs.dropdown', function() {
+  if (window.matchMedia("(max-width: 991.98px)").matches) {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown(slideSpeed);
+  } else {
+    $(this).find('.dropdown-menu').first().stop(true, true).show("slide", {
+      direction: 'up'
+    }, slideSpeed);
+  }
+});
+$('.jqui-slide').on('hide.bs.dropdown', function() {
+  if (window.matchMedia("(max-width: 991.98px)").matches) {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp(slideSpeed);
+  } else {
+    $(this).find('.dropdown-menu').first().stop(true, true).hide("slide", {
+      direction: 'up'
+    }, slideSpeed);
+  }
+});
